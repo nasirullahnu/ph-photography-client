83da7fee2a3215c5e433fcd60f59905f5217681c
nasirullahnu <nasirullah878@gmail.com>
2022-11-10 03:44:33 +0600

    stat component added

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Banner from '../../HomeItems/Banner/Banner';
 import HomeServices from '../../HomeItems/HomeServices/HomeServices';
+import Stat from '../../HomeItems/Stat/Stat';
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
             <HomeServices></HomeServices>
+            <Stat></Stat>
         </div>
     );
 };
